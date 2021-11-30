49b93d63a4c39b11622e57356641af8d72ea728b
thisancog <matthias@matthias-planitzer.de>
2021-11-30 20:30:52 +0100

    Added more settings in the control panel, including a seeded random generator

--- src/js/main.js
+++ src/js/main.js
@@ -44,18 +44,31 @@
 	var loom = function() {
 		var sketchContainer = document.querySelector('.sketch-container'),
 			controls        = document.querySelector('.controls'),
+			settings        = controls.querySelector('.settings'),
+			settingsToggle  = settings.querySelector('.settings-toggle'),
 			addWeftBtn      = controls.querySelector('.add-weft-btn'),
 			removeWeftBtns  = [].slice.call(controls.querySelectorAll('.setting-wefts .remove-btn')),
+			addWarpBtn      = controls.querySelector('.add-warp-btn'),
+			removeWarpBtns  = [].slice.call(controls.querySelectorAll('.setting-warps .remove-btn')),
+			seedInput       = controls.querySelector('#seed'),
 			generateBtn     = controls.querySelector('#generate'),
 			downloadBtn     = controls.querySelector('#download'),
 			args, p5Sketch;
 
 		var registerEvents = function() {
-			addWeftBtn.addEventListener('click', addWeft);
-			removeWeftBtns.forEach(btn => btn.addEventListener('click', removeWeft));
-			generateBtn.addEventListener('click', resetLoom);
-			downloadBtn.addEventListener('click', saveFrame);
-			controls.addEventListener('submit', resetLoom);
+			settingsToggle.addEventListener('click', toggleSettings);
+			addWeftBtn.addEventListener(    'click', addWeft);
+			removeWeftBtns.forEach(btn =>
+				btn.addEventListener(       'click', removeWeft)
+			);
+			addWarpBtn.addEventListener(    'click', addWarp);
+			removeWarpBtns.forEach(btn =>
+				btn.addEventListener(       'click', removeWarp)
+			);
+
+			generateBtn.addEventListener(   'click', resetLoom);
+			downloadBtn.addEventListener(   'click', saveFrame);
+			controls.addEventListener(     'submit', resetLoom);
 		}
 
 		var initLoom = function() {
@@ -73,28 +86,80 @@
 		}
 
 		var parseArgs = function() {
+			if (seedInput.value.length > 0)
+				generateFromString(seedInput.value);
+
 			let threadWidth   = parseFloat(document.querySelector('#thread-width').value),
 				threadSpacing = parseFloat(document.querySelector('#thread-spacing').value),
 				patternWidth  = parseInt(document.querySelector('#pattern-width').value);
 
-			let weftColors    = [].slice.call(controls.querySelectorAll('.setting-wefts .repeater-elements input[type="color"]')).
-								map(input => input.value);
+			let weftColors    = [].slice.call(controls.querySelectorAll('.setting-wefts .repeater-elements input[type="color"]'))
+								  .map(input => input.value);
+
+			let warps         = [].slice.call(controls.querySelectorAll('.setting-warps .repeater-elements .repeater-single'))
+								  .map(function(row) {
+										return {
+											color:   row.querySelector('input[type="color"]').value,
+											pattern: row.querySelector('input[type="text"]').value
+													    .replace(/[^01]/g, '')
+													    .split(''),
+								  		}
+								  });
 
 			return {
 				threadWidth: 	threadWidth,
 				threadSpacing:	threadSpacing,
 				threadTotal:    threadWidth + threadSpacing,
 				patternWidth:	patternWidth,
-				weftColors: 	weftColors     
+				weftColors: 	weftColors,
+				warpSettings:   warps 
 			};
 		}
+
+		var generateFromString = function(value) {
+			let threadWidthInput   = document.querySelector('#thread-width'),
+				threadSpacingInput = document.querySelector('#thread-spacing'),
+				patternWidth       = document.querySelector('#pattern-width');
+
+			//	clear everything
+			document.querySelector('.setting-wefts .repeater-elements').innerHTML = '';
+			document.querySelector('.setting-warps .repeater-elements').innerHTML = '';
+
+
+			let getRandomNumber = xmur3(value),
+				numWefts        = parseInt(lerp(getRandomNumber(), 1, 50)),
+				numWarps        = parseInt(lerp(getRandomNumber(), 1, 50)),
+				threadWidth     = parseInt(lerp(getRandomNumber(), 1, 50));
+
+			threadWidthInput.value   = threadWidth;
+			threadSpacingInput.value = parseInt(lerp(getRandomNumber(), 0, 0.5 * threadWidth));
+			patternWidth.value       = parseInt(lerp(getRandomNumber(), 1, 999));
+
+			for (let i = 0; i < numWefts; i++) {
+				let newColor = generateRandomColor(getRandomNumber);
+				addWeft(newColor);
+			}
+
+			for (let i = 0; i < numWarps; i++) {
+				let newColor      = generateRandomColor(getRandomNumber),
+					patternLength = parseInt(lerp(getRandomNumber(), 4, 200)),
+					newPattern    = '';
+
+				for (let n = 0; n < patternLength; n++) {
+					newPattern = newPattern + (getRandomNumber() < 0.5 ? '0' : '1');
+				}
+
+				addWarp(newColor, newPattern);
+			}
+		}
+
 
 
 		/***************************************
 			Interface
 		***************************************/
 
-		var addWeft = function() {
+		var addWeft = function(color) {
 			let weftsInner = controls.querySelector('.setting-wefts .repeater-elements'),
 				template   = controls.querySelector('.setting-wefts .repeater-template'),
 				newElem    = template.cloneNode(true),
@@ -103,6 +168,10 @@
 			newElem.classList.remove('repeater-template');
 			weftsInner.appendChild(newElem);
 			removeBtn.addEventListener('click', removeWeft);
+
+			if (color !== null) {
+				newElem.querySelector('input[type="color"]').value = color;
+			}
 		}
 
 		var removeWeft = function(e) {
@@ -111,11 +180,40 @@
 			row.parentElement.removeChild(row);
 		}
 
+		var addWarp = function(color, pattern) {
+			let warpsInner = controls.querySelector('.setting-warps .repeater-elements'),
+				template   = controls.querySelector('.setting-warps .repeater-template'),
+				newElem    = template.cloneNode(true),
+				removeBtn  = newElem.querySelector('.remove-btn');
+
+			newElem.classList.remove('repeater-template');
+			warpsInner.appendChild(newElem);
+			removeBtn.addEventListener('click', removeWarp);
+
+			if (color !== null) {
+				newElem.querySelector('input[type="color"]').value = color;
+			}
+
+			if (pattern !== null) {
+				newElem.querySelector('input[type="text"]').value = pattern;
+			}
+		}
+
+		var removeWarp = function(e) {
+			let row = e.target.closest('.repeater-single');
+			e.target.removeEventListener('click', removeWarp);
+			row.parentElement.removeChild(row);
+		}
+
 		var saveFrame = function() {
 			let now      = new Date(),
 				fileName = 'loom_' + now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate()
 						 + ' ' + now.getHours() + '-' + now.getMinutes() + '-' + now.getSeconds();
 			p5Sketch.save(fileName + '.jpg');
+		}
+
+		var toggleSettings = function() {
+			settings.classList.toggle('show-settings');
 		}
 
 
@@ -127,19 +225,7 @@
 		var sketch = function(p) {
 			let wefts         = [],
 				numWefts,
-
-				warps         = [],
-				warpSettings  = [
-					{
-						color: 		'#777',
-						pattern: 	[ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
-					},
-					{
-						color: 		'#777',
-						pattern: 	[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 ]
-					}
-				],
-				numWarps      = warpSettings.length;
+				warps         = [];
 
 			var	offsetX          = 0.5 * args.threadWidth,
 				currentY         = 0,
@@ -159,14 +245,14 @@
 					}));
 				}
 
-				for (let y = 0; y < numWarps; y++) {
+				for (let y = 0; y < args.warpSettings.length; y++) {
 					warps.push(new Warp({
-						color:  	warpSettings[y].color,
+						color:  	args.warpSettings[y].color,
 						gap:        args.threadSpacing,
 						step:       args.threadTotal,
 						width:  	args.threadWidth,
 						numWefts:   numWefts,
-						pattern:	warpSettings[y].pattern
+						pattern:	args.warpSettings[y].pattern
 					}));
 				}
 
@@ -190,7 +276,7 @@
 					 	wefts[x].weave(currentY);
 				}
 
-				currentWarpIndex = wrapIndex(currentWarpIndex + 1, 0, numWarps - 1);
+				currentWarpIndex = wrapIndex(currentWarpIndex + 1, 0, args.warpSettings.length - 1);
 				currentY++;
 			}
 
@@ -247,23 +333,51 @@
 				}
 			}
 
-
-
-			/***************************************
-				Helper functions
-			***************************************/
-
 			var getNumWefts = function() {
 				let minWidth      = Math.floor(p.width / args.threadTotal),
 					maxWidth      = Math.ceil(minWidth / args.patternWidth) * args.patternWidth + Math.floor(0.5 * args.patternWidth);
 				return maxWidth;
 			}
+		}
 
-			var wrapIndex = function(i, min, max) {
-				if (i >= min && i <= max)	return i;
-				if (i > max)				return min;
-											return max;
+		/***************************************
+			Helper functions
+		***************************************/
+
+		var wrapIndex = function(n, min, max) {
+			if (n >= min && n <= max)	return n;
+			if (n > max)				return min;
+										return max;
+		}
+
+		var limit = function(n, min, max) {
+			return Math.min(Math.max(n, min), max);
+		}
+
+		var lerp = function(n, min, max) {
+        	return min + (max - min) * limit(n, 0, 1);
+		}
+
+		// seeded pseudo-random number generator
+		var xmur3 = function(seed) {
+			for (var i = 0, h = 1779033703 ^ seed.length; i < seed.length; i++) {
+				h = Math.imul(h ^ seed.charCodeAt(i), 3432918353);
+				h = h << 13 | h >>> 19;
 			}
+
+			return function() {
+				h = Math.imul(h ^ h >>> 16, 2246822507);
+				h = Math.imul(h ^ h >>> 13, 3266489909);
+				h = (h ^= h >>> 16) >>> 0;
+				return h / Math.pow(10, h.toString().length);
+			}
+		}
+
+		var generateRandomColor = function(PRNG) {
+			let r = parseInt(lerp(PRNG(), 0, 255)),
+				g = parseInt(lerp(PRNG(), 0, 255)),
+				b = parseInt(lerp(PRNG(), 0, 255));
+			return '#' + r.toString(16) + g.toString(16) + b.toString(16);
 		}
 
 		registerEvents();
